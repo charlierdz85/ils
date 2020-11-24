54b10317233922864b7dfd9d4223509a5bef3755
Charly <shiru_jan@hotmail.com>
2020-11-23 21:15:16 -0600

    First commit

---
+++ ILS/src/main/java/com/ils/IlsApplication.java
@@ -0,0 +1,13 @@
+package com.ils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlsApplication.class, args);
+	}
+
+}
